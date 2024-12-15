a0311cabce24aa4c8fc92d47526c42175bcc7f44
7382022 <amreenrida2@gmail.com>
2024-12-15 10:19:10 -0500

    Vechile

---
+++ class Vechile.py
@@ -0,0 +1,13 @@
+class Vechile:
+
+    def __init__(self,max_speed,mileage):
+        self.max_speed = max_speed
+        self.mileage= mileage
+
+
+
+
+modelX = Vechile(240,18)
+
+print("Model ,max speed:",modelX.max_speed)
+print("Model ,mileage:",modelX.mileage)
